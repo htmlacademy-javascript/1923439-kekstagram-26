c85b34448131f49d800f1adceda6965f55775c6d
Вася Чехов <4exoff@novoeradio.by>
2022-06-02 15:12:24 +0300

    Написал функции для генерации случайного числа и для провверки длинны комментария (жду подтверждения от Дениса)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+const MAX_LENGTH_COMMENT = 140;
 
+// Генерация случайного числа
+const getRandomNumber = function (firstNumber, secondNumber) {
+  return Math.floor(Math.random() * (secondNumber - firstNumber)) + firstNumber;
+};
+
+getRandomNumber(3,80);
+
+// Проверка длинны комментария
+const checksLengthComment = function (currentLengthComment) {
+  if (currentLengthComment > MAX_LENGTH_COMMENT) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+checksLengthComment(25);
+
+//
