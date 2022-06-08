97cad7d815d9bc1be0939025c78574aa59d31842
Вася Чехов <4exoff@novoeradio.by>
2022-06-08 11:47:37 +0300

    Сделал более прозрачный цикл создания массива объектов, ID - последовательны, без повторений. URL - последовательны, без повторений. Комментарии склеиваются из двух рандомно

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const MAX_LENGTH_COMMENT = 140;
 const USERS_PHOTOS_COUNT = 25;
 
 // Данные придуманные мной
-const namesAutors = [
+const NAMES_AUTORS = [
   'Иван',
   'Платон',
   'Евлампий',
@@ -19,14 +19,15 @@ const namesAutors = [
   'Галя',
   'Зина'
 ];
-const comments = [
+const COMMENTS = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.'
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'В целом всё неплохо. Но не всё.'
 ];
 
-const descriptions = [
+const DESCRIPTIONS = [
   'Упал лицом на кнопку "снять"',
   'Моя Мама говорит мне, что у меня есть талант',
   'Продаётся снимок - дорого',
@@ -38,7 +39,7 @@ const descriptions = [
 // Генерация случайного числа
 const getPositiveRandomInt = function (min, max) {
   if (max > min && min >= 0 && max >= 0) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
     throw new Error ('Число должно быть положительным и/или больше минимального значения!');
   }
@@ -56,22 +57,34 @@ const getRandomArrayElement = function (elements) {
   return elements[getPositiveRandomInt(0, elements.length - 1)];
 };
 
-// Функция для создания массива с обектами фотографий пользователей
+// Функция для создания объекта фотографий пользователей
 const getPhotos = function () {
   return  {
-    id: getPositiveRandomInt(1, 25),
+    id: ' ',
     url: `photos/${getPositiveRandomInt(1, 25) }.jpg`,
-    description: getRandomArrayElement(descriptions),
+    description: getRandomArrayElement(DESCRIPTIONS),
     likes: getPositiveRandomInt(15, 200),
     comments: [
       {
-        id: getPositiveRandomInt(1, 200),
+        id: ' ',
         avatar: `img/avatar-${getPositiveRandomInt(1, 6)}.svg`,
-        message: getRandomArrayElement(comments),
-        name: getRandomArrayElement(namesAutors)
+        message: ' ',
+        name: getRandomArrayElement(NAMES_AUTORS)
       }
     ]
   };
 };
 
-const photos = Array.from({length: USERS_PHOTOS_COUNT}, getPhotos);
+// Собираем массив объектов
+const photosArray = [];
+for (let i = 1; i <= USERS_PHOTOS_COUNT; i++) {
+  photosArray[i] = getPhotos();
+  photosArray[i].id = i;
+  photosArray[i].url = `photos/${i}.jpg`;
+  photosArray[i].comments.id = getPositiveRandomInt(1, 200);
+  photosArray[i].comments.message = COMMENTS[getPositiveRandomInt(0, COMMENTS.length - 1)];
+  if (getPositiveRandomInt(1, 2) > 1) {
+    photosArray[i].comments.message = `${COMMENTS[getPositiveRandomInt(0, COMMENTS.length - 1)] } ${  COMMENTS[getPositiveRandomInt(0, COMMENTS.length - 1)]}`;
+  }
+}
+
