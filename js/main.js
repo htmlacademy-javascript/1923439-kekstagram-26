9caad624fac3771b3213344f076f24d80e6a32b6
Вася Чехов <4exoff@novoeradio.by>
2022-06-21 14:41:56 +0300

    Начал выполнять первое задание 7-го раздела

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {getPhotosArray, USERS_PHOTOS_COUNT} from './data.js';
 import {checksLength, MAX_LENGTH_COMMENT} from './util.js';
 import './comments.js';
 import './util.js';
+import './users_miniatures';
 
 checksLength('Я бы не стал таким заниматься', MAX_LENGTH_COMMENT);
 getPhotosArray(USERS_PHOTOS_COUNT);
