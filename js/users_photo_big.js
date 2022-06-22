8f5ef7615cd43ce39e01cbc1fac76bf704d6c2e6
Вася Чехов <4exoff@novoeradio.by>
2022-06-22 21:21:26 +0300

    Во время консультации сделали отображение больших картинок идентичных нажатой миниатюре, осталось отрисовать комментарии с аватарками

--- js/users_photo_big.js
+++ js/users_photo_big.js
@@ -1,38 +1,40 @@
-// // Находим коллекцию сгенерированных миниатюр
-// const miniPhotos = document.querySelectorAll('.picture');
+// Находим секцию детального просмотра фотографий
+const bigPictureSection = document.querySelector('.big-picture');
 
-// // Находим секцию детального просмотра фотографий
-// const bigPictureSection = document.querySelector('.big-picture');
+// Находим кнопку закрытия окна просмотра большой фотографии
+const bigPictureClose = bigPictureSection.querySelector('.big-picture__cancel');
 
-// // Находим кнопку закрытия окна просмотра большой фотографии
-// const bigPictureClose = bigPictureSection.querySelector('.big-picture__cancel');
-
-// // Функция закрывающая окно просмотра большой фотографии кнопкой 'Esc'
-// const getCloseBigPictureKeyBoard = () => {
-//   document.addEventListener('keydown', (evt) => {
-//     if (evt.keyCode === 27) {
-//       bigPictureSection.classList.add('hidden');
-//     }
-//   });
-// };
-
-// // Функция закрывающая окно просмотра большой фотографии  кликом по кнопке закрытия
-// const getCloseBigPictureClick = () => {
-//   bigPictureClose.addEventListener('click', () => {
-//     bigPictureSection.classList.add('hidden');
-//   });
-// };
+// Находим контейнер с большой фотографией
+const bigPicture = bigPictureSection.querySelector('.big-picture__img img');
+const likesCount = bigPictureSection.querySelector('.likes-count');
+const commentsCount = bigPictureSection.querySelector('.comments-count');
+const photoDescription = bigPictureSection.querySelector('.social__caption');
 
 
-// // Функция для выделения одной миниатюры из массива и открытия окна просмотра большой фотографии
-// const getOpenBigPhoto = () => {
-//   for (const miniPhoto of miniPhotos) {
-//     miniPhoto.addEventListener('click', (evt) => {
-//       evt.preventDefault();
-//       bigPictureSection.classList.remove('hidden');
-//       getCloseBigPictureKeyBoard();
-//       getCloseBigPictureClick();
-//     });
-//   }
-// };
+// Функция закрывающая окно просмотра большой фотографии кнопкой 'Esc'
+const getCloseBigPictureKeyBoard = () => {
+  document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) {
+      bigPictureSection.classList.add('hidden');
+    }
+  });
+};
 
+// Функция закрывающая окно просмотра большой фотографии  кликом по кнопке закрытия
+const getCloseBigPictureClick = () => {
+  bigPictureClose.addEventListener('click', () => {
+    bigPictureSection.classList.add('hidden');
+  });
+};
+
+const getPhotosInfo = ({url, likes, comments, description}) => {
+  bigPicture.src = url;
+  likesCount.textContent = likes;
+  commentsCount.textContent = comments.length;
+  photoDescription.textContent = description;
+  bigPictureSection.classList.remove('hidden');
+  getCloseBigPictureKeyBoard();
+  getCloseBigPictureClick();
+};
+
+export {bigPicture, getPhotosInfo};
