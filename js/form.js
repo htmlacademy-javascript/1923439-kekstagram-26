dc9ada56ad1e6e59931ef1f56a8a29eb6896c8e6
Вася Чехов <4exoff@novoeradio.by>
2022-06-28 21:25:03 +0300

    Внёс правки после консультации, почти всё сделано, есть нюанс с проверкой на определённую символику

--- js/form.js
+++ js/form.js
@@ -12,18 +12,14 @@ const editPopupCLoseButton = document.querySelector('#upload-cancel');
 // Находим форму для загрузки фотографии пользователем
 const userPhotoForm = document.querySelector('.img-upload__form');
 
-// Находим значение инпута с хэштегами
-const hushtagsFieldValue = userPhotoForm.querySelector('.text__hashtags').value;
-
 // Находим инпут с хэштегами
 const hushtagsField = userPhotoForm.querySelector('.text__hashtags');
 
 // Находим инпут с комментариями
 const commentField = userPhotoForm.querySelector('.text__description');
 
-
 // Добавляем обработчик показа формы редактирования изображения
-uploadFileInput.addEventListener('change', openEditPhotosPopap);
+uploadFileInput.addEventListener('change', openEditPhotosPopup);
 
 // Остановка обработчика на поле в фокусе
 const stopListenerOnFocus = (firstObject, secondObject) => {
@@ -49,7 +45,7 @@ const onEditPopupClick = () => {
 };
 
 // функция открытия попапа редактирования фотографии
-function openEditPhotosPopap () {
+function openEditPhotosPopup () {
   formPhotoEdit.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
   editPopupCLoseButton.addEventListener('click', onEditPopupClick);
@@ -72,7 +68,7 @@ const pristine = new Pristine(userPhotoForm, {
   errorClass: 'img-upload__field-wrapper--invalid',
   successClass: 'img-upload__field-wrapper--valid',
   errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'span',
+  errorTextTag: 'div',
   errorTextClass: 'img-upload__form__error'
 });
 
@@ -80,44 +76,43 @@ const pristine = new Pristine(userPhotoForm, {
 // Функция проверки длинны комментария
 const checkCommentLength = (commentsValue) => commentsValue.length <= MAX_LENGTH_COMMENT;
 
+checkCommentLength(commentField);
+
 // Функция валидации символики и длинны хэштега
-const checkHushtagsLength = (hushtagsValue) => {
+const checkHushtagsSymbol = (hushtagsValue) => {
   const regularHustags = /^#[A-Za-zА-Яа-яЁё0-9]{2,19}$/i;
-  return regularHustags.test(hushtagsValue) || hushtagsValue === ' ';
+  return regularHustags.test(hushtagsValue) || hushtagsValue === '';
 };
 
 // Функция валидации уникальности и колличества хэштегов
 const checkHushtagsUniq = (hushtagsValue) => {
-  const regularHustags = /^#[A-Za-zА-Яа-яЁё0-9]{2,19}(\s#[A-Za-zА-Яа-яЁё0-9]{2,19}){0,4}$/i;
-  const hushtagsArray = hushtagsValue.split(' ');
-  const unicArray = new Set(hushtagsArray);
-  return unicArray.size === hushtagsArray.length && regularHustags.test(hushtagsValue);
+  const hushtagsArray = hushtagsValue.trim().toLowerCase().split(' ');
+  const mayArray = new Set(hushtagsArray);
+  // console.log(mayArray.size, hushtagsArray.length);
+  // console.log(hushtagsValue);
+  return mayArray.size === hushtagsArray.length && hushtagsArray.length <= 5;
 };
 
-checkHushtagsUniq(hushtagsFieldValue);
+const checkHashtagsLength = (hashtagsLengts) => hashtagsLengts.length >= 2 && hashtagsLengts.length <= 20;
 
-pristine.addValidator(userPhotoForm.querySelector('.text__hashtags',
-  checkHushtagsLength,
-  'Хэштеги должен быть не длиннее 20 символов, начинаться с # и разделяться пробелом'));
+pristine.addValidator(userPhotoForm.querySelector('.text__hashtags'),
+  checkHashtagsLength,
+  'Хэштег должен быть более 2 символов и не длиннее 20!');
 
-pristine.addValidator(userPhotoForm.querySelector('.text__hashtags',
+pristine.addValidator(userPhotoForm.querySelector('.text__hashtags'),
+  checkHushtagsSymbol,
+  'Хэштег должен начинаться с # и разделяться пробелом');
+
+pristine.addValidator(userPhotoForm.querySelector('.text__hashtags'),
   checkHushtagsUniq,
-  'максимум 5 уникальных хэштегов!'));
+  'Максимум 5 уникальных хэштегов!');
 
-pristine.addValidator(userPhotoForm.querySelector('.text__description',
+pristine.addValidator(userPhotoForm.querySelector('.text__description'),
   checkCommentLength,
-  'Не более 140 символов'));
+  'Не более 140 символов');
 
 userPhotoForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
-//   // const usersHushtagsField = userPhotoForm.querySelector('.text__hashtags').value;
-//   // const regularHustags = /^#[A-Za-zА-Яа-яЁё0-9]{2,19}(\s#[A-Za-zА-Яа-яЁё0-9]{2,19}){0,4}$/i;
-//   // const isValid = pristine.validate();
-//   // if (isValid && regularHustags.test(usersHushtagsField) || usersHushtagsField === '') {
-//   //   console.log ('good form');
-//   // } else {
-//   //   console.log('invalid form');
-//   // }
 });
 
