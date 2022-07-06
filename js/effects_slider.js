fca59c8827c0187cb99410644ed80eaa96eea9c0
Вася Чехов <4exoff@novoeradio.by>
2022-07-06 22:06:34 +0300

    Сделал слайдер эффектов как надо

--- js/effects_slider.js
+++ js/effects_slider.js
@@ -3,12 +3,7 @@ import {imgPreview} from './scale_size_photos.js';
 const sliderContainer = document.querySelector('.img-upload__effect-level');
 const sliderElement = document.querySelector('.effect-level__slider');
 const sliderValue = document.querySelector('.effect-level__value');
-const chromeEffect = document.querySelector('#effect-chrome');
-const sepiaEffect = document.querySelector('#effect-sepia');
-const marvinEffect = document.querySelector('#effect-marvin');
-const phobosEffect = document.querySelector('#effect-phobos');
-const heatEffect = document.querySelector('#effect-heat');
-const originalEffect = document.querySelector('#effect-none');
+
 
 // Создаём слайдер
 noUiSlider.create(sliderElement, {
@@ -32,94 +27,94 @@ noUiSlider.create(sliderElement, {
   },
 });
 
+
 // Создаём словарь
-const sliderOptionsList = {
-  min: [0, 1],
-  max: [1, 100, 3],
-  step: [0.1, 1],
-  start: [1, 100, 3],
-  effects: ['grayscale', 'sepia', 'invert', 'blur', 'brightness'],
-  unit: ['%', 'px', ''],
+const filters = {
+  chrome: {
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      step: 0.1,
+      start: 1,
+    },
+    filter: (value) => `grayscale(${value})`,
+  },
+  sepia: {
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      step: 0.1,
+      start: 1,
+    },
+    filter: (value) => `sepia(${value})`,
+  },
+  marvin: {
+    options: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      step: 1,
+      start: 100,
+    },
+    filter: (value) => `invert(${value}%)`,
+  },
+  phobos: {
+    options: {
+      range: {
+        min: 0,
+        max: 3,
+      },
+      step: 0.1,
+      start: 3,
+    },
+    filter: (value) => `blur(${value}px)`,
+  },
+  heat: {
+    options: {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      step: 0.1,
+      start: 3,
+    },
+    filter: (value) => `brightness(${value})`,
+  },
+  none: {
+    options: {
+      range: {
+        min: 1,
+        max: 100,
+      },
+      step: 1,
+      start: 100,
+    },
+    filter: () => 'none',
+  },
 };
 
 // Универсальная функция добавления и регулирования эффектов фотографий
-const renderPhotoEffect = (min, max, step, start, effects, unit) => {
+const renderPhotoEffect = (object) => {
   sliderContainer.classList.remove('hidden');
-  sliderElement.noUiSlider.updateOptions ({
-    range: {
-      min: min,
-      max: max,
-    },
-    step: step,
-    start: start,
-  });
+  sliderElement.noUiSlider.updateOptions (object.options);
   sliderElement.noUiSlider.on('update', () => {
     sliderValue.value = sliderElement.noUiSlider.get();
-    imgPreview.style.filter = `${effects}(${sliderValue.value}${unit})`;
+    imgPreview.style.filter = object.filter(sliderValue.value);
   });
 };
 
-// Скрытие слайдера на оригинальном фильтре при открытии страницы
-if (originalEffect.checked) {
-  sliderContainer.classList.add('hidden');
-}
-
 // функция отслеживания изменений в родительской форме
-const onFormChange = (evt) => {
-  if (evt.target.matches('input[id="effect-chrome"]')) {
-    renderPhotoEffect(
-      sliderOptionsList.min[0],
-      sliderOptionsList.max[0],
-      sliderOptionsList.step[0],
-      sliderOptionsList.start[0],
-      sliderOptionsList.effects[0],
-      sliderOptionsList.unit[2]);
-    imgPreview.className = `effects__preview--${chromeEffect.value}`;
-  }
-  if (evt.target.matches('input[id="effect-sepia"]')) {
-    renderPhotoEffect(
-      sliderOptionsList.min[0],
-      sliderOptionsList.max[0],
-      sliderOptionsList.step[0],
-      sliderOptionsList.start[0],
-      sliderOptionsList.effects[1],
-      sliderOptionsList.unit[2]);
-    imgPreview.className = `effects__preview--${sepiaEffect.value}`;
-  }
-  if (evt.target.matches('input[id="effect-marvin"]')) {
-    renderPhotoEffect(
-      sliderOptionsList.min[0],
-      sliderOptionsList.max[1],
-      sliderOptionsList.step[1],
-      sliderOptionsList.start[1],
-      sliderOptionsList.effects[2],
-      sliderOptionsList.unit[0]);
-    imgPreview.className = `effects__preview--${marvinEffect.value}`;
-  }
-  if (evt.target.matches('input[id="effect-phobos"]')) {
-    renderPhotoEffect(
-      sliderOptionsList.min[0],
-      sliderOptionsList.max[2],
-      sliderOptionsList.step[0],
-      sliderOptionsList.start[2],
-      sliderOptionsList.effects[3],
-      sliderOptionsList.unit[1]);
-    imgPreview.className = `effects__preview--${phobosEffect.value}`;
-  }
-  if (evt.target.matches('input[id="effect-heat"]')) {
-    renderPhotoEffect(
-      sliderOptionsList.min[1],
-      sliderOptionsList.max[2],
-      sliderOptionsList.step[0],
-      sliderOptionsList.start[2],
-      sliderOptionsList.effects[4],
-      sliderOptionsList.unit[2]);
-    imgPreview.className = `effects__preview--${heatEffect.value}`;
-  }
-  if (evt.target.matches('input[id="effect-none"]')) {
+const onFormChange = () => {
+  const checkedFilter = document.querySelector('input[name="effect"]:checked').value;
+  imgPreview.className = `effects__preview--${checkedFilter}`;
+  renderPhotoEffect(filters[checkedFilter]);
+  if (checkedFilter === 'none') {
     sliderContainer.classList.add('hidden');
-    imgPreview.style.filter = 'none';
-    imgPreview.className = `effects__preview--${originalEffect.value}`;
   }
 };
 
