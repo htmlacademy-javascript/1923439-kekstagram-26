ac827b707f77dfeb93dc5351518fef63f3d6f9e1
Keks <keks@htmlacademy.ru>
2022-07-15 23:10:28 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/sorting_filter.js
+++ 12/js/sorting_filter.js
@@ -1,11 +1,11 @@
-import {shuffleArray, deleteActiveClassFromButton} from './util.js';
-import {renderUsersMiniatures} from './users_photo_miniatures.js';
+import {shuffleArray, debounce} from './util.js';
+import './users_photo_miniatures.js';
+import { renderUsersMiniatures } from './users_photo_miniatures.js';
 
 const MAX_RANDOM_PHOTOS_COUNT = 10;
+const DEBOUNCE_TIMER = 1500;
 const sortingFilter = document.querySelector('.img-filters');
-const defaultButton = sortingFilter.querySelector('#filter-default');
 const randomButton = sortingFilter.querySelector('#filter-random');
-const discussedButton = sortingFilter.querySelector('#filter-discussed');
 const sortingButtons = sortingFilter.querySelectorAll('.img-filters__button');
 
 // показываем меню сортировки
@@ -17,25 +17,15 @@ const showSortingFilter = () => {
 const compareComments = (commentA, commentB) => commentB.comments.length - commentA.comments.length;
 
 // Функция показа рандомных фоток с сервера
-const showRandomPhoto = (photos) => {
-  deleteActiveClassFromButton(sortingButtons, 'img-filters__button--active');
-  randomButton.classList.add('img-filters__button--active');
+const showRandomPhoto = debounce((photos) => {
   const shuffeledArray = shuffleArray(photos.slice());
   renderUsersMiniatures(shuffeledArray.slice(0, MAX_RANDOM_PHOTOS_COUNT));
-};
+}, DEBOUNCE_TIMER);
 
 // Функция показа всех фоток с сервера по умолчанию
-const showDefaultPhoto = (photos) => {
-  deleteActiveClassFromButton(sortingButtons, 'img-filters__button--active');
-  defaultButton.classList.add('img-filters__button--active');
-  renderUsersMiniatures(photos);
-};
+const showDefaultPhoto = debounce((photos) => renderUsersMiniatures(photos), DEBOUNCE_TIMER);
 
 // Функция показа самых обсуждаемых фоток
-const showDiscussedPhoto = (photos) => {
-  deleteActiveClassFromButton(sortingButtons, 'img-filters__button--active');
-  discussedButton.classList.add('img-filters__button--active');
-  renderUsersMiniatures(photos.slice().sort(compareComments));
-};
+const showDiscussedPhoto = debounce((photos) => renderUsersMiniatures(photos.slice().sort(compareComments)), DEBOUNCE_TIMER);
 
-export {showSortingFilter, showRandomPhoto, showDefaultPhoto, showDiscussedPhoto, randomButton};
+export {showSortingFilter, showRandomPhoto, showDefaultPhoto, showDiscussedPhoto, randomButton, sortingButtons};
